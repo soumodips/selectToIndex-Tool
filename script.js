8acc213f4fe875edcac10e575b64b578fa18ef57
Soumodip Sinha <M1041753@mindtree.com>
2018-02-23 19:55:44 +0530

    Upgraded for multiple utterances at a time

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(function () {
 
+    $("#tbl").hide();
+    $("#btnDeleteRow").hide();
+    $("#btnSubmit").hide();
+    $("#btnSave").hide();
     var uttIdx = 0;
-    $('#btnAddUtterance').click(function () {
+    $('#btnAddUtterance').click(() => {
         populateUtterance();
     });
 
@@ -15,26 +19,37 @@ $(function () {
 
     let userUtterance;
     let selectedTexts = []
+    let allutterance = []
     $('#contents').mouseup(() => {
         if(window.getSelection().toString()){    
             var selObj = window.getSelection().toString().trim();
             var start = userUtterance.indexOf(selObj);
             var end = (start + selObj.length);
-            selectedTexts.push(`{"entity": "","value": "","start": `+start+`,"end": `+end+`}`);
+            selectedTexts.push(`{"entity": "`+selObj+`","value": "","start": `+start+`,"end": `+end+`}`);
             console.log(selectedTexts);
         }
     });
     function populateUtterance() {
+        $("#tbl").show();
+        $("#btnDeleteRow").show();
+        $("#btnSubmit").show();
+        $("#btnSave").show();
         userUtterance = $('#myInput').val();
-        $("#contents").html(userUtterance);
+        $("#contents").append(`<tr><td>select</td><td>`+userUtterance+`</td><td>output</td></tr>`);
         $('#myInput').val('');
     }
 
-    $('#btnSubmit').click(function (e) {
+    $('#btnSubmit').click((e) => {
         if(selectedTexts !== "[]")
-        saveText( selectedTexts , "selectedTexts.json" );
+        saveText( allutterance , "allUtterances.json" );
         else
         alert("Dont forget to select first!")
+    });
+    
+    $('#btnSave').click(() =>{
+        //TODO save logic
+        allutterance.push(`{"text": "`+userUtterance+`","intent": "extract-content","entities": [`+selectedTexts+`]}`);
+        selectedTexts=[];
     });
 
     function saveText(text, fileName) {
